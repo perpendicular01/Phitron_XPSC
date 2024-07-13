f359346d2bf5002e394b61d2e0eb1357aa1061dc
saif <kh.s.k.rohan@gmail.com>
2024-07-14 00:06:40 +0600

    13 july

--- Week 9/C-Concert-Tickets.cpp
+++ Week 9/C-Concert-Tickets.cpp
@@ -3,7 +3,34 @@ using namespace std;
 #define ll long long int
 
 void solve(){
+    int n, m;
+    cin >> n >> m;
+
+    multiset<int, greater<int>> arr;
+    vector<ll> b(m);
     
+    for(int i=0; i<n; i++){
+        int a;
+        cin >> a;
+        arr.insert(a); 
+    }
+
+    for(int i=0; i<m; i++){
+        cin >> b[i];
+    }
+  
+    for(int i=0; i<m; i++){
+        auto it = arr.lower_bound(b[i]);
+
+        if(it == arr.end()){
+            cout << -1 << endl;
+        }
+        else{
+            cout << *it << endl;
+            arr.erase(it);
+        }
+    }
+
 }
 
 
